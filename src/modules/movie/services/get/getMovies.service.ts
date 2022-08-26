9f8d7f9bac4fb85cb980fbad42b69efd16af8b09
Lazaro Manuell <manuelvianacunga@gmail.com>
2022-08-26 11:57:14 +0100

    feat: :sparkles: add getMovies service

---
+++ src/modules/movie/services/get/getMovies.service.ts
@@ -0,0 +1,17 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { MoviesRepository } from 'src/infra/database/typeorm/repositories/movies/moviesRepository';
+import { Movies } from '../../entities/movie';
+
+@Injectable()
+export class GetMovieService {
+  constructor(private movieRepository: MoviesRepository) {}
+
+  async execute(id: string): Promise<Movies> {
+    const movies = await this.movieRepository.getById(id);
+
+    if (!movies) {
+      throw new NotFoundException('Filme não encontrado.');
+    }
+    return movies;
+  }
+}
