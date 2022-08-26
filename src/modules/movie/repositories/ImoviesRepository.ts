7c94d2140c2ff7380bec1708ec589fb9e074c3f6
Lazaro Manuell <manuelvianacunga@gmail.com>
2022-08-26 12:08:13 +0100

    movie repository interface

---
+++ src/modules/movie/repositories/ImoviesRepository.ts
@@ -0,0 +1,11 @@
+import { CreateMovieDto } from '../dtos/createMovies.dto';
+import { Movies } from '../entities/movie';
+
+export interface IMoviesRepository {
+  create(data: CreateMovieDto): Promise<Movies>;
+  findByTitle(title: string): Promise<Movies>;
+  findAll(): Promise<Movies[]>;
+  update(id: string, data: CreateMovieDto): Promise<Movies>;
+  getById(id: string): Promise<Movies>;
+  delete(id: Movies): Promise<void>;
+}
