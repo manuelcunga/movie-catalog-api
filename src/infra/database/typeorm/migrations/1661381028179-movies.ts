d1b7560554e8cf86402bec959a94b660a99730fc
Lazaro Manuell <manuelvianacunga@gmail.com>
2022-08-26 12:00:39 +0100

    add movies migrations

---
+++ src/infra/database/typeorm/migrations/1661381028179-movies.ts
@@ -0,0 +1,44 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class movies1661381028179 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'movies',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            isGenerated: true,
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+          },
+          {
+            name: 'director',
+            type: 'varchar',
+          },
+          {
+            name: 'year',
+            type: 'date',
+          },
+          {
+            name: 'score',
+            type: 'integer',
+          },
+          {
+            name: 'createdAt',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('movies');
+  }
+}
