b5d8ac33050ebd048cac10115897324e919f60df
Lazaro Manuell <manuelvianacunga@gmail.com>
2022-08-26 12:01:07 +0100

    add movies repository database

---
+++ src/infra/database/typeorm/repositories/users/UserRepositories.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { IUserRepository } from 'src/modules/accounts/repositories/IUserRepositories';
+import { User } from 'src/modules/accounts/entities/User';
+import { CreateUserDTO } from 'src/modules/accounts/dtos/createUserDTO';
+import { UpdateUserDTO } from 'src/modules/accounts/dtos/updateUser.dto';
+import { LoginDTO } from 'src/modules/accounts/dtos/login.dto';
+
+@Injectable()
+export class UserRepository implements IUserRepository {
+  constructor(
+    @InjectRepository(User)
+    private readonly ormRepository: Repository<User>,
+  ) {}
+
+  public async create(data: CreateUserDTO): Promise<User> {
+    const user = this.ormRepository.create(data);
+    await this.ormRepository.save(user);
+    return user;
+  }
+
+  public async findByEmail(email: string) {
+    return await this.ormRepository.findOne({ email });
+  }
+
+  public async findAllUsers(): Promise<User[]> {
+    const users = await this.ormRepository.find();
+    return users;
+  }
+
+  public async update(id: string, data: UpdateUserDTO): Promise<User> {
+    const user = await this.ormRepository.preload({
+      id,
+      ...data,
+    });
+
+    return await this.ormRepository.save(user);
+  }
+
+  public async find(user: LoginDTO): Promise<User> {
+    const Finduser = await this.ormRepository.findOne(user);
+    return Finduser;
+  }
+}
