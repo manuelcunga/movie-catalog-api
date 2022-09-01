3635d09dea4e6d2e76a67aa4095c5d3752390b54
Lazaro Manuell <manuelvianacunga@gmail.com>
2022-09-01 09:54:21 +0100

    refactor: :fire: refatorando

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,29 +1,46 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { CacheInterceptor, CacheModule, Module } from '@nestjs/common';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { UserModule } from './modules/accounts/user.module';
 import { MoviesModule } from './modules/movie/movies.module';
 import { ConfigModule } from '@nestjs/config';
+import * as redisStore from 'cache-manager-redis-store';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [
+    CacheModule.registerAsync({
+      useFactory: () => {
+        return {
+          store: redisStore,
+          host: process.env.REDIS_HOST,
+          port: process.env.REDIS_PORT,
+          ttl: 60 * 3600 * 1000,
+        };
+      },
+    }),
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'Docker',
-      password: 'root',
-      database: 'movie',
-      entities: ['dist/src/modules/**/entities/*.js'],
-      migrations: ['dist/src/infra/database/typeorm/migrations/*.js'],
+      type: process.env.TYPEORM_CONNECTION,
+      host: process.env.TYPEORM_HOST,
+      port: process.env.TYPEORM_PORT,
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
+      entities: ['./dist/src/modules/**/entities/*.js'],
+      migrations: ['./dist/src/infra/database/typeorm/migrations/*.js'],
       migrationRun: true,
       synchronize: true,
       logging: false,
       autoLoadEntities: true,
-    }),
+    } as TypeOrmModuleOptions),
     UserModule,
     MoviesModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+  ],
 })
 export class AppModule {}
