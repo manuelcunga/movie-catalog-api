4207d8cc604610bc2388b5f1f6a1ace5c595dd76
Lazaro Manuell <manuelvianacunga@gmail.com>
2022-09-01 09:58:27 +0100

    feat: :sparkles: add login service

--- src/modules/accounts/services/login/login.service.ts
+++ src/modules/accounts/services/login/login.service.ts
@@ -1,46 +1,33 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 import { UserRepository } from 'src/infra/database/typeorm/repositories/users/UserRepositories';
-import { LoginDTO } from '../../dtos/login.dto';
 
 @Injectable()
 export class LoginService {
   constructor(
-    private createUserRepository: UserRepository,
+    private usersRepository: UserRepository,
     private jwtService: JwtService,
   ) {}
 
-  async execute(data: LoginDTO): Promise<string> {
-    const user = await this.createUserRepository.findByEmail(data.email);
+  async login(user) {
+    const payload = { sub: user.id, email: user.email };
 
-    if (!user) {
-      throw new BadRequestException('Email or password incorrect');
-    }
-
-    const passwordCompare = bcrypt.compareSync(data.password, user.password);
-
-    if (!passwordCompare) {
-      throw new BadRequestException('Email or password incorrect');
-    }
-
-    const payload = {
-      sub: user.id,
-      email: user.email,
-      role: user.phone,
+    return {
+      token: this.jwtService.sign(payload, {
+        secret: process.env.JWT_KEY,
+        expiresIn: '60s',
+      }),
     };
-
-    return this.jwtService.sign(payload);
   }
 
-  async validateCredentials(data: LoginDTO) {
-    const user = await this.createUserRepository.findByEmail(data.email);
+  async validateUser(email: string, password: string) {
+    const user = await this.usersRepository.findByEmail(email);
 
-    if (!user) {
-      throw new BadRequestException('Email or password incorrect');
-    }
+    const isPasswordValid = bcrypt.compareSync(password, user.password);
 
-    const passwordCompare = bcrypt.compareSync(data.password, user.password);
-    return passwordCompare;
+    if (!isPasswordValid) return null;
+
+    return user;
   }
 }
