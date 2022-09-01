e2a8d05475387de349bf1b62c7d6e459c6c4d1f8
Lazaro Manuell <manuelvianacunga@gmail.com>
2022-09-01 09:59:01 +0100

    refator create movies controller add auth

--- src/modules/movie/controller/create/createMovie.controller.ts
+++ src/modules/movie/controller/create/createMovie.controller.ts
@@ -4,12 +4,15 @@ import {
   HttpCode,
   HttpStatus,
   Post,
+  UseGuards,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { CreateMovieDto } from '../../dtos/createMovies.dto';
 import { CreateMovieService } from '../../services/create/createMovies.service';
 
+@UseGuards(AuthGuard('jwt'))
 @Controller('movies')
 export class CreateMoviesController {
   constructor(private readonly createMovieService: CreateMovieService) {}
