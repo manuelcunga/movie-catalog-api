d1e297834fad585a4a98f76ce91aee7852604ba6
Lazaro Manuell <manuelvianacunga@gmail.com>
2022-09-01 09:59:34 +0100

    refactor: :fire: refatorando list all add auth

--- src/modules/movie/controller/listAll/listAllMovie.controller.ts
+++ src/modules/movie/controller/listAll/listAllMovie.controller.ts
@@ -1,11 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  CacheInterceptor,
+  CacheKey,
+  CacheTTL,
+  Controller,
+  Get,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { ListAllMoviesService } from '../../services/listAll/listallMovies.service';
 
+@UseGuards(AuthGuard('jwt'))
 @Controller('movies')
 export class ListAllMoviesController {
   constructor(private readonly moviesService: ListAllMoviesService) {}
 
   @Get('/')
+  @UseInterceptors(CacheInterceptor)
+  @CacheKey('movies-redis')
+  @CacheTTL(30)
   async handle() {
     return await this.moviesService.execute();
   }
